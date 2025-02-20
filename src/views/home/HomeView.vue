b860841197dfd193a2d3d1008984c476a8bc3c72
tecinfoifc <monitoriainfoifc2024@gmail.com>
2025-02-20 18:44:24 -0300

    CHORE: Just change my components to test view

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,13 +1,3 @@
-<script setup>
-    import GlobalText from '@/components/global/titles/GlobalText.vue';
-</script>
 <template>
-    <div class="p-5 flex flex-col gap-10">
-        <GlobalText text="Sejam bem Vindos ao IFC Campus Araquari" :has_gradient="true"/>
-        <GlobalText text="Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer" width="w-2/5" spacing="leading-7" size="text-md" weight="font-normal"/>
-    </div>
-    <div class="p-5 flex flex-col gap-10">
-        <GlobalText text="O que você precisa saber sobre o IF?" color="text-blue-600" />
-        <GlobalText text="Mudanças de horários, olimpiadas estudantis, segunda chamada, sábados letivos entre outros. É calouro? Veja os primeiros passos:" width="w-2/5" spacing="leading-7" size="text-xl" weight="font-bold" text_align="text-justify"/>
-    </div>
+    <h1>Anthony viado</h1>
 </template>
--- src/views/tests/TestComponents.vue
+++ src/views/tests/TestComponents.vue
@@ -1,6 +1,6 @@
 <script setup>
 import GlobalButton from '@/components/global/buttons/GlobalButton.vue';
-
+import GlobalText from '@/components/global/titles/GlobalText.vue';
 function envieCoisa() {
     console.log('coisa')
 }
@@ -10,5 +10,13 @@ function envieCoisa() {
 <template>
     <div class="w-1/4">
         <GlobalButton content="Login" :icon="false" @evento="envieCoisa"/>
+    </div>
+    <div class="p-5 flex flex-col gap-10">
+        <GlobalText text="Sejam bem Vindos ao IFC Campus Araquari" :has_gradient="true"/>
+        <GlobalText text="Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer" width="w-2/5" spacing="leading-7" size="text-md" weight="font-normal"/>
+    </div>
+    <div class="p-5 flex flex-col gap-10">
+        <GlobalText text="O que você precisa saber sobre o IF?" color="text-blue-600" />
+        <GlobalText text="Mudanças de horários, olimpiadas estudantis, segunda chamada, sábados letivos entre outros. É calouro? Veja os primeiros passos:" width="w-2/5" spacing="leading-7" size="text-xl" weight="font-bold" text_align="text-justify"/>
     </div>
 </template>
