e407038e0f58bf82ceac6a75b276cfbdbffa621c
tecinfoifc <monitoriainfoifc2024@gmail.com>
2025-02-20 18:29:48 -0300

    FEAT:Title&Subtitle default Component created #35

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import GlobalButton from "./global/buttons/GlobalButton.vue";
 import PostCard from "./global/cards/PostCard.vue";
 import PostContainerCard from "./global/cards/PostContainerCard.vue";
 import PostForm from "./tests/PostForm.vue";
+import GlobalText from "./global/titles/GlobalText.vue";
 
 export {
     GlobalButton,
     PostCard,
     PostContainerCard,
-    PostForm
+    PostForm,
+    GlobalText
 }
