e40ac4f2c5f5c0d516ca29cc92342b10f756690d
tecinfoifc <monitoriainfoifc2024@gmail.com>
2025-02-21 20:41:18 -0300

    FEAT: Footer created #39

---
+++ src/components/layout/footer/Footer.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { links, linksIFC, linksContact } from '@/utils';
+import { GlobalText } from '@/components';
+
+const images = import.meta.glob('@/assets/images/*', { eager: true });
+
+const getImageUrl = (imgName) => {
+  return images[`/src/assets/images/${imgName}`]?.default || '';
+};
+
+</script>
+<template>
+    <footer class="bg-[#347EBF] flex  justify-between items-center text-white p-10">
+            <div class="w-6/12 flex justify-center flex-col gap-10 p-5 ">
+                <img src="../../../assets/images/Aurora footer.png" width="110" alt="">
+                <GlobalText text="Bons estudos! agredecemos a visita" size="text-[28px]" spacing="leading-12" width="w-2/5" weight="font-semibold"/>
+                <img src="../../../assets/images/iflogo.png" width="289" alt="">
+            </div>
+        <div class="flex gap-20 p-5">
+            <div class="flex flex-col gap-7">
+                <div>
+                    <h1 class="font-bold">Navegue</h1>
+                </div>
+                <div class="flex flex-col gap-3">
+                    <router-link v-for="link, i in links" :key="i" :to="link.link">{{ link.name }}</router-link>
+                </div>
+            </div>
+            <div class="flex flex-col gap-7" >
+                <div>
+                    <h1 class="font-bold">Acesso rápido</h1>
+                </div>
+                <div class="flex flex-col gap-3">
+                    <a v-for="linksif, i in linksIFC" :href="linksif.link" :key="i">{{ linksif.name }}</a>
+                </div>
+            </div>
+            <div class="flex flex-col gap-7">
+                <div>
+                    <h1 class="font-bold">Precisa de Ajuda?</h1>
+                </div>
+                <div class="flex gap-3 items-center">
+                    <img src="../../../assets/images/telephone-call.png" class="h-4"/>
+                    <span>55 47 98765-6789</span>
+                </div>
+                <div class="flex gap-7">
+                    <a v-for="linkcont, i in linksContact" :key="i" :href="linkcont.link">
+                        <img :src="getImageUrl(linkcont.img)" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
--- src/utils/links/links.js
+++ src/utils/links/links.js
@@ -20,3 +20,39 @@ export const links = [
         link: 'about/'
     }
 ]
+
+export const linksIFC = [{
+    name: 'Site Ifc Araquari',
+    link: 'https://araquari.ifc.edu.br/'
+},
+{
+    name: 'Sigaa',
+    link: 'https://sig.ifc.edu.br/sigaa/'
+},
+{
+    name: 'Sepe',
+    link: 'https://eventos.ifc.edu.br/sepe/'
+},
+{
+    name: 'Bolsas',
+    link: 'https://estudante.ifc.edu.br/2024/02/15/edital-no-4-2024-programa-de-auxilios-estudantis-pae/'
+},
+{
+    name: 'Horário Academico',
+    link: 'https://horarios.araquari.ifc.edu.br/data/horario2025.1_years_days_horizontal.html'
+}]
+
+export const linksContact = [
+    {
+        img: 'whatsapp.png',
+        link: 'https://api.whatsapp.com/send/?phone=554738037273&text=Ol%C3%A1+%2ACampus+Araquari%2A%21+Preciso+de+informa%C3%A7%C3%B5es+sobre+o+IFC.&type=phone_number&app_absent=0'
+    },
+    {
+        img: 'facebook.png',
+        link: 'https://www.facebook.com/ifc.oficial.araquari/'
+    },
+    {
+        img: 'instagram.png',
+        link: 'https://www.instagram.com/ifc.oficial.araquari/'
+    }
+]
