5e1e30d98c3a8001510e39d64908abda2b5d9362
AnthonyLoche <anthonylocheifc@gmail.com>
2025-02-24 18:20:31 -0300

    FEAT: Make the main comp to home [Refs: #42]

---
+++ src/components/aurora/home/main/MainSection.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-[95%] mx-auto  py-16 px-4 font-sans">
+        <div class="w-full mx-auto">
+            <div class="flex flex-col md:flex-row justify-between items-center gap-12 mb-24">
+                <MainText />
+                <MainImages />
+            </div>
+        </div>
+        <MainCourses />
+    </div>
+</template>
+
+<script setup>
+import MainCourses from './MainCourses.vue';
+import MainImages from './MainImages.vue';
+import MainText from './MainText.vue';
+</script>
---
+++ src/components/aurora/home/main/MainText.vue
@@ -0,0 +1,24 @@
+// HeroWelcome.vue
+<template>
+    <div class="w-full md:w-1/2">
+        <h1 class="text-5xl md:text-6xl lg:text-8xl font-bold text-gray-900 leading-tight mb-8 gradientBlue">
+            Sejam Bem Vindos<br />
+            ao IFC Campus<br />
+            Araquari
+        </h1>
+        <p class="text-sm md:text-base text-black mb-8 max-w-2xl">
+            Is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+            has been the industry's standard dummy text ever since the 1500s, when an
+            unknown printer
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.gradientBlue {
+    background: linear-gradient(90deg, #000000 0%, #347EBF 97.5%);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+</style>
