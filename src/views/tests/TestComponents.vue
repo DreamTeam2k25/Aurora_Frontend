31c7e5eacf5cef943e7a06c28b16540e30467eb4
jaotarzan <joaovssouza59@gmail.com>
2025-02-24 13:43:20 -0300

    FEAT: Create the mini guild members diplay [Refs: #37]

--- src/views/tests/TestComponents.vue
+++ src/views/tests/TestComponents.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { MiniGuildMembers } from '@/components';
 import GlobalButton from '@/components/global/buttons/GlobalButton.vue';
 import GlobalText from '@/components/global/titles/GlobalText.vue';
 import Header from '@/components/layout/header/Header.vue';
@@ -21,4 +22,7 @@ function envieCoisa() {
         <GlobalText text="Mudanças de horários, olimpiadas estudantis, segunda chamada, sábados letivos entre outros. É calouro? Veja os primeiros passos:" width="w-2/5" spacing="leading-7" size="text-xl" weight="font-bold" text_align="text-justify"/>
     </div>
     <Header />
+    <div class="p-5 flex flex-col gap-10">
+        <MiniGuildMembers current-guild="exemplo" />
+    </div>
 </template>
