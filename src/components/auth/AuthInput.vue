411114b36cc768d2dfb834299dcf40847792035f
Gabz047 <gabe1612mail@gmail.com>
2025-03-03 15:54:39 -0300

    FEAT: Made component responsive

--- src/components/auth/AuthInput.vue
+++ src/components/auth/AuthInput.vue
@@ -19,7 +19,7 @@ const authMethod = [
     <div v-for="(data, index) in authData" :key="index" class="p-2">
         <p class="p-2">{{ data.title }}:</p>
         <div>
-            <input class=" border-[0.1px] border-[#C7C7C7] w-[312px] py-3 px-5 rounded-full outline-0" type="text" :placeholder="data.placeholder">
+            <input class=" border-[0.1px] border-[#C7C7C7] w-[312px] max-md:w-12/12 max-md:text-[13px] py-3 px-5 rounded-full outline-0" type="text" :placeholder="data.placeholder">
         </div>
     </div>
 
