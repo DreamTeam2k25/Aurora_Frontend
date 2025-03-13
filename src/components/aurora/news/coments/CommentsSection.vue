04df1f9fa78c0e7970cd48eda30db9f50ff22d94
luansilva25 <luansilva250807@gmail.com>
2025-03-13 17:58:03 -0300

    FEAT: create chat comment component [refs:#75]

---
+++ src/components/aurora/news/coments/CommentsSection.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import { ChatComment } from '@/components';
+    const user = "Anthony"
+
+    const users = ['teste', 'teste2', 'teste3']
+</script>
+<template>
+<section class="p-2">
+    <ChatComment :user="user" />
+    <ChatComment v-for="user, i in users" :key="i" :is-chat="true" :user="user"/>
+</section>
+</template>
