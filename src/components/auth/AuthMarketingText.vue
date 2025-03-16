9286f3855680c75f8b3e9c297ad36e70cd852cac
Gabz047 <gabe1612mail@gmail.com>
2025-03-16 13:49:15 -0300

    FIX: fixed items justift

--- src/components/auth/AuthMarketingText.vue
+++ src/components/auth/AuthMarketingText.vue
@@ -27,15 +27,14 @@ const text_content = computed(() => ({
         content: '#VemViveroIFC'
     },
     hasbeforecontent: props.isLogin ? true : false,
-    justify: props.isLogin ? true : false
 }));
 </script>
 
 <template>
- <div :class="`flex flex-col absolute p-10 items-${ text_content.justify ? 'start' : 'end'}`">
+ <div :class="`flex flex-col absolute p-10 items-${ props.isLogin ? 'start' : 'end'}`">
     <h2 :class="` text-2xl font-semibold text-white leading-12`" >{{ text_content.firstShortText.content }}</h2>
-    <h1  :class="` text-6xl font-semibold text-[#82B0D9] leading-20 ${ text_content.hasbeforecontent ? ` texto-first` : ``} `">{{ text_content.firstMainText.content }}</h1>
-    <h1  :class="` text-6xl font-semibold text-[#82B0D9] leading-20 ${text_content.hasbeforecontent ? ` texto-second` : ``} `">{{ text_content.secondMainText.content }}</h1>
+    <h1  :class="` text-6xl font-semibold text-[#82B0D9] leading-20 ${ props.isLogin ? ` texto-first` : ``} `">{{ text_content.firstMainText.content }}</h1>
+    <h1  :class="` text-6xl font-semibold text-[#82B0D9] leading-20 ${props.isLogin ? ` texto-second` : ``} `">{{ text_content.secondMainText.content }}</h1>
     <h1  :class="` text-6xl font-semibold text-[#82B0D9] leading-20`">{{ text_content.thirdMainText.content }}</h1>
     <h2  :class="` text-2xl font-semibold leading-14 text-white`">{{ text_content.secondShortText.content }}</h2>
  </div>
