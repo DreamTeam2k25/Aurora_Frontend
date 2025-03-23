a3ae89b24014def9e1adcbc954a44e1f66e43a5d
Gabz047 <gabe1612mail@gmail.com>
2025-03-23 11:44:24 -0300

    FEAT: Created Pagination Component and added it into ombdusmanView

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 import { links, linksIFC, linksContact } from "./links/links";
 import { findData } from "./auth/auth";
+import { amountOfPages } from "./pagination/pagination";
 
 export {
     links,
     linksIFC,
     linksContact,
     findData,
+    amountOfPages,
 }
