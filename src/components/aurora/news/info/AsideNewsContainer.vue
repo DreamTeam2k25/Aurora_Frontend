1cbbac57163b1134bb78b95457276f78dd5c835a
jaotarzan <joaovssouza59@gmail.com>
2025-03-24 16:25:08 -0300

    FEAT: Creating the aside news component [Refs #71]

---
+++ src/components/aurora/news/info/AsideNewsContainer.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { SimpleNewsCard, GlobalText } from "@/components";
+
+const posts = [
+    {
+        img: "https://th.bing.com/th/id/R.47e099307c72b3c8b98cecd028a93bd7?rik=Ffm0wYsWHxGZeA&riu=http%3a%2f%2fnoset.com.br%2fwp-content%2fuploads%2f2016%2f09%2fgato-2.jpg&ehk=FH9enUmzg8ElklSWMMfepJThXqOE%2fdH1oSWFHrr6WDo%3d&risl=&pid=ImgRaw&r=0",
+        type: "esportes",
+        title: "Os Benefícios do Exercício Diário",
+        description: "Praticar esportes regularmente melhora a saúde.",
+        publishedAt: "10 de Fevereiro, 2025",
+    },
+    {
+        img: "https://th.bing.com/th/id/R.47e099307c72b3c8b98cecd028a93bd7?rik=Ffm0wYsWHxGZeA&riu=http%3a%2f%2fnoset.com.br%2fwp-content%2fuploads%2f2016%2f09%2fgato-2.jpg&ehk=FH9enUmzg8ElklSWMMfepJThXqOE%2fdH1oSWFHrr6WDo%3d&risl=&pid=ImgRaw&r=0",
+        type: "esportes",
+        title: "Os Benefícios do Exercício Diário",
+        description: "Praticar esportes regularmente melhora a saúde.",
+        publishedAt: "10 de Fevereiro, 2025",
+    },
+    {
+        img: "https://th.bing.com/th/id/R.47e099307c72b3c8b98cecd028a93bd7?rik=Ffm0wYsWHxGZeA&riu=http%3a%2f%2fnoset.com.br%2fwp-content%2fuploads%2f2016%2f09%2fgato-2.jpg&ehk=FH9enUmzg8ElklSWMMfepJThXqOE%2fdH1oSWFHrr6WDo%3d&risl=&pid=ImgRaw&r=0",
+        type: "esportes",
+        title: "Os Benefícios do Exercício Diário",
+        description: "Praticar esportes regularmente melhora a saúde.",
+        publishedAt: "10 de Fevereiro, 2025",
+    },
+    {
+        img: "https://th.bing.com/th/id/R.47e099307c72b3c8b98cecd028a93bd7?rik=Ffm0wYsWHxGZeA&riu=http%3a%2f%2fnoset.com.br%2fwp-content%2fuploads%2f2016%2f09%2fgato-2.jpg&ehk=FH9enUmzg8ElklSWMMfepJThXqOE%2fdH1oSWFHrr6WDo%3d&risl=&pid=ImgRaw&r=0",
+        type: "esportes",
+        title: "Os Benefícios do Exercício Diário",
+        description: "Praticar esportes regularmente melhora a saúde.",
+        publishedAt: "10 de Fevereiro, 2025",
+    },
+    {
+        img: "https://th.bing.com/th/id/R.47e099307c72b3c8b98cecd028a93bd7?rik=Ffm0wYsWHxGZeA&riu=http%3a%2f%2fnoset.com.br%2fwp-content%2fuploads%2f2016%2f09%2fgato-2.jpg&ehk=FH9enUmzg8ElklSWMMfepJThXqOE%2fdH1oSWFHrr6WDo%3d&risl=&pid=ImgRaw&r=0",
+        type: "esportes",
+        title: "Os Benefícios do Exercício Diário",
+        description: "Praticar esportes regularmente melhora a saúde.",
+        publishedAt: "10 de Fevereiro, 2025",
+    },
+]
+</script>
+
+<template>
+    <div class="flex flex-col items-center w-1/4 my-5">
+        <div class="w-full flex justify-start pl-9 my-5">
+            <GlobalText size="text-2xl" text="notícias recentes" weight="font-semibold" spacing="" width="w-auto" />
+        </div>
+        <div v-for="(post, id) in posts" :key="id" class="w-76 my-4">
+            <SimpleNewsCard :post="post" />
+        </div>
+    </div>
+</template> 
---
+++ src/components/global/cards/SimpleNewsCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+    post: {
+        type: Object,
+        required: true,
+        default: () => ({
+            img: '',
+            type: 'geral',
+            title: '',
+            description: '',
+            publishedAt: ''
+        })
+    },
+})
+</script>
+
+<template>
+    <div :class="'rounded-[25px] overflow-hidden border border-gray-300 bg-white shadow-sm p-4 pt-6 pb-8 pl-4 w-[95%] h-50'">
+        <div :class="'relative h-24 w-full overflow-hidden rounded-[25px]'" >
+            <img :src="props.post.img" :alt="props.post.title" :class="'h-full w-full object-cover rounded-[15px]'"  />
+            <div class="absolute bottom-[20px] left-5 z-10">
+                <span style="font-weight: 600;" :class="'bg-[#82B0D9] text-[#000] p-2 rounded-[15px] text-sm capitalize' ">
+                    {{ props.post.type }}
+                </span>
+            </div>
+        </div>
+        <div :class="'p-4 space-y-3'">
+            <h3 :style="{ fontWeight: 600 || 'normal' }" :class="'text-normal text-gray-900 line-clamp-2'">
+                {{ props.post.title }}
+            </h3>
+        </div>
+    </div>
+</template>
